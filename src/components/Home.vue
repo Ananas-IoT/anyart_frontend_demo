533661dd860f11886a857f5823dec31bc2cd6b40
KulianTaras <kuliantaras@gmail.com>
2019-12-08 18:54:01 +0200

    i don't know what i'm doing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,17 @@
 <template>
-    <div style="padding: 0" class="grey darken-2">
+    <div style="padding: 0" class="grey darken-2 white--text">
         <v-container fluid style="padding: 0; margin: 0">
             <v-row class="images-box" style="padding: 0; margin: 0">
                 <v-col v-touch="top" cols="4" style="padding: 0">
                     <v-img alt="image"
-                           src="https://i.pinimg.com/originals/c2/58/b9/c258b9e36ce904484ec145251f5ed792.jpg"></v-img>
+                           src="@/assets/c258b9e36ce904484ec145251f5ed792.jpg"></v-img>
                 </v-col>
                 <v-col v-touch="top" cols="4" style="padding: 0">
-                    <v-img alt="image" src="https://i.pinimg.com/originals/10/b4/54/10b45425c209218d122b916c505fe1f9.jpg"></v-img>
+                    <v-img alt="image" src="@/assets/10b45425c209218d122b916c505fe1f9.jpg"></v-img>
                 </v-col>
                 <v-col v-touch="top" cols="4" style="padding: 0">
                     <v-img alt="image"
-                           src="https://res.cloudinary.com/fleetnation/image/private/c_fit,w_1120/g_south,l_text:style_gothic2:%C2%A9%20SeventyFour,o_20,y_10/g_center,l_watermark4,o_25,y_50/v1513101520/y2jycss8btwpwuskhilm.jpg"></v-img>
+                           src="@/assets/y2jycss8btwpwuskhilm.jpg"></v-img>
                 </v-col>
             </v-row>
             <v-row>
@@ -20,17 +20,52 @@
                         <h1 class="display-1 mx-auto pa-0">ТУТ МАЄ БУТИ ЯКАСЬ ПРИКОЛЬНА ЦИТАТКА</h1>
                     </v-card-title>
                 </v-card>
-            </v-row>`
-        </v-container>
-        <v-container>
-            <v-card span class="block-about-project grey darken-2 white--text" style="box-shadow: none">
-                <v-card-title class="center-title">
-                    <h2 class="center-title mx-auto">ЦЕ ПЛАТФОРМА БЛА БЛА БЛА БЛА</h2>
-                </v-card-title>
-                <v-card-title class="center-title">
-                    <h3 class="center-title mx-auto">HOW IT WORKS?</h3>
-                </v-card-title>
-            </v-card>
+            </v-row>
+            <v-container>
+                <v-card span class="block-about-project grey darken-2 white--text" style="box-shadow: none">
+                    <v-card-title class="center-title">
+                        <h2 class="center-title mx-auto">ЦЕ ПЛАТФОРМА БЛА БЛА БЛА БЛА</h2>
+                    </v-card-title>
+                    <v-card-title class="center-title">
+                        <h3 class="center-title mx-auto">HOW IT WORKS?</h3>
+                    </v-card-title>
+                </v-card>
+            </v-container>
+            <v-row>
+                <v-col cols="2" style="text-align: center">
+                    <v-spacer></v-spacer>
+                </v-col>
+                <v-col cols="6" style="text-align: center">
+                    <img src="@/assets/tarasphoto.jpg" alt="image" style="width: 1000px">
+                </v-col>
+                <v-col cols="4" style="text-align: center">
+                    <v-spacer></v-spacer>
+                </v-col>
+            </v-row>
+            <h1 class="center-title mx-auto">ABOUT US</h1>
+            <h2 class="center-title mx-auto">We are happy to see you on our site! Our purpose is to make Lviv a better
+                place for all of us</h2>
+                <v-row>
+
+                    <v-col cols="4" style="text-align: center">
+                        <v-avatar style="margin: 0 auto" size="300">
+                            <img src="@/assets/photo_2019-11-15_23-52-00.jpg" alt="John">
+                        </v-avatar>
+                    </v-col>
+
+                    <v-col cols="4" style="text-align: center">
+                        <v-avatar size="300">
+                            <img src="@/assets/photo_2019-11-15_23-52-00.jpg" alt="John">
+                        </v-avatar>
+                    </v-col>
+
+                    <v-col cols="4" style="text-align: center">
+                        <v-avatar size="300">
+                            <img src="@/assets/photo_2019-11-15_23-52-00.jpg" alt="John">
+                        </v-avatar>
+                    </v-col>
+
+                </v-row>
         </v-container>
     </div>
 </template>
@@ -48,6 +83,7 @@
         position: relative;
         z-index: 1;
     }
+
     .main-title {
         font-size: 30px;
         position: absolute;
@@ -58,9 +94,11 @@
         left: 50px;
         text-align: center;
     }
+
     .block-about-project {
         position: relative;
     }
+
     .center-title {
         text-align: center;
     }
